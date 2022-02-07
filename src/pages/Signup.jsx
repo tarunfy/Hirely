ab4773911a95f07b32bffae2f7442a0452f916cf
tarunfy <tarunsharma8920@gmail.com>
2022-02-07 23:14:00 +0530

    created applicant form on signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,17 @@ import React, { useState } from "react";
 import Grid from "../assets/grid.svg";
 
 const Signup = () => {
+  const [role, setRole] = useState("Recruiter");
+  const [gender, setGender] = useState("");
+  const [experienceLevel, setExperienceLevel] = useState("");
+  const [dob, setDob] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [company, setCompany] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="flex h-screen justify-between items-center bg-slate-50">
       <div className="px-4 py-8 ml-16 mt-10 w-128 rounded-lg shadow-material bg-slate-50">
@@ -9,17 +20,23 @@ const Signup = () => {
           <div className="flex items-center space-x-1">
             <input
               type="radio"
-              checked
+              checked={role === "Recruiter"}
               id="recruiter"
+              onClick={() => setRole("Recruiter")}
               name="role"
-              className="bg-slate-50"
             />
             <label htmlFor="recruiter" className="text-lg">
               Register as recruiter
             </label>
           </div>
           <div className="flex items-center space-x-1">
-            <input type="radio" id="applicant" name="role" />
+            <input
+              type="radio"
+              id="applicant"
+              name="role"
+              onClick={() => setRole("Applicant")}
+              checked={role === "Applicant"}
+            />
             <label htmlFor="applicant" className="text-lg">
               Register as applicant
             </label>
@@ -30,41 +47,95 @@ const Signup = () => {
             <input
               type="text"
               className="border-2 p-2 w-full rounded-md"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
               placeholder="Full Name"
               required
             />
           </div>
           <div className="input-container mb-5">
-            <input
-              type="text"
-              className="border-2 p-2 w-full rounded-md"
-              placeholder="My Company"
-              required
-            />
+            {role === "Recruiter" ? (
+              <input
+                type="text"
+                className="border-2 p-2 w-full rounded-md"
+                value={company}
+                onChange={(e) => setCompany(e.target.value)}
+                placeholder="My Company"
+                required
+              />
+            ) : (
+              <input
+                type="text"
+                className="border-2 p-2 w-full rounded-md"
+                value={experienceLevel}
+                onChange={(e) => setExperienceLevel(e.target.value)}
+                placeholder="Experience Level"
+                required
+              />
+            )}
           </div>
           <div className="input-container mb-5">
-            <input
-              type="text"
-              className="border-2 p-2 w-full rounded-md"
-              placeholder="Designation"
-              required
-            />
+            {role === "Recruiter" ? (
+              <input
+                type="text"
+                className="border-2 p-2 w-full rounded-md"
+                value={designation}
+                onChange={(e) => setDesignation(e.target.value)}
+                placeholder="Designation"
+                required
+              />
+            ) : (
+              <input
+                type="text"
+                className="border-2 p-2 w-full rounded-md"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+                placeholder="Gender"
+                required
+              />
+            )}
           </div>
           <div className="input-container mb-5">
-            <input
-              type="email"
-              className="border-2 p-2 w-full rounded-md"
-              placeholder="Email"
-              required
-            />
+            {role === "Recruiter" ? (
+              <input
+                type="email"
+                className="border-2 p-2 w-full rounded-md"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+              />
+            ) : (
+              <input
+                type="date"
+                className="border-2 p-2 w-full rounded-md"
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+                placeholder="Date of birth"
+                required
+              />
+            )}
           </div>
           <div className="input-container mb-5">
-            <input
-              type="password"
-              className="border-2 p-2 w-full rounded-md"
-              placeholder="Password"
-              required
-            />
+            {role === "Recruiter" ? (
+              <input
+                type="password"
+                className="border-2 p-2 w-full rounded-md"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+              />
+            ) : (
+              <input
+                type="number"
+                className="border-2 p-2 w-full rounded-md"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                placeholder="Phone Number"
+                required
+              />
+            )}
           </div>
           <button className="bg-primary-600 uppercase hover:bg-primary-700 transition-colors duration-300 ease-in-out w-full text-lg font-bold px-5 py-2 rounded-md shadow-md shadow-primary-400 text-white">
             Register
