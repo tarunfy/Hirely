4703ad62961590608e3d2c7e09ca7c128edd0053
tarunfy <tarunsharma8920@gmail.com>
2022-02-16 23:27:00 +0530

    Configured env variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCAIWVrefkO1b1J_a9ni-DIby7tdYAhq_s",
-  authDomain: "hirely-29309.firebaseapp.com",
-  projectId: "hirely-29309",
-  storageBucket: "hirely-29309.appspot.com",
-  messagingSenderId: "807640873025",
-  appId: "1:807640873025:web:9332d5fb93453e1d2c2ec0"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
