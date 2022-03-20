01431c89b505e97f27b9f05bf3957f1b0cb7f0f7
tarunfy <tarunsharma8920@gmail.com>
2022-03-20 21:08:30 +0530

    addded apply job button and removed applications count from job listing on applicant side

--- src/components/Panels/Applicant.jsx
+++ src/components/Panels/Applicant.jsx
@@ -155,15 +155,17 @@ const Applicant = () => {
                         {job.jobDescription}
                       </p>
                     </div>
-                    <div>
-                      <h3>Applications: 50</h3>
-                    </div>
                   </div>
                   <div className="flex flex-col justify-between text-right">
                     <div>{moment(job.createdAt).format("MM/DD/YYYY")}</div>
                     <div className="flex items-center justify-end space-x-5">
+                      <button className="group relative inline-flex border border-secondary-600 focus:outline-none w-full sm:w-auto">
+                        <span className="w-full inline-flex items-center justify-center self-stretch px-4 py-2 text-sm text-white text-center font-bold uppercase bg-secondary-600 ring-1 ring-secondary-600 ring-offset-1 ring-offset-secondary-600 transform transition-transform group-hover:-translate-y-1 group-hover:-translate-x-1 group-focus:-translate-y-1 group-focus:-translate-x-1">
+                          Apply
+                        </span>
+                      </button>
                       <Tippy content="View" inertia animation="scale">
-                        <button className="border-[1px] border-black p-2">
+                        <button className="border-[1px] focus:outline-none border-black p-2">
                           <VisibilityIcon />
                         </button>
                       </Tippy>
