a4bb933459d48457cf736d7ab87a7f206546637a
tarunfy <tarunsharma8920@gmail.com>
2022-03-21 19:52:32 +0530

    changes

--- src/pages/AppliedJobs.jsx
+++ src/pages/AppliedJobs.jsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { JobContext } from "../contexts/JobContext";
 import NavigationBar from "../components/Navbar";
 import AppliedJobCard from "../components/AppliedJobCard";
@@ -21,12 +22,22 @@ const AppliedJobs = () => {
   return (
     <>
       <NavigationBar />
-      <div className="grid grid-cols-3 gap-x-4 gap-y-5 pt-36 px-16 bg-slate-50 h-screen w-full">
-        {appliedJobss &&
-          appliedJobss.map((job, index) => (
+      {appliedJobss && appliedJobss.length > 0 ? (
+        <div className="grid grid-cols-3 gap-x-4 gap-y-5 pt-36 px-16 bg-slate-50 h-screen w-full">
+          {appliedJobss.map((job, index) => (
             <AppliedJobCard key={index} job={job} />
           ))}
-      </div>
+        </div>
+      ) : (
+        <div className="pt-60 h-screen">
+          <h1 className="text-center text-xl font-medium">
+            You have not applied to any jobs yet,{" "}
+            <Link to="/dashboard" className="text-secondary-600 font-bold">
+              Apply now
+            </Link>
+          </h1>
+        </div>
+      )}
     </>
   );
 };
