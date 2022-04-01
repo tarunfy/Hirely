eb5cbb8db40561e397ab35750a897f0189c32984
tarunfy <tarunsharma8920@gmail.com>
2022-04-01 17:55:23 +0530

    changes in applications page

--- src/pages/Applications.jsx
+++ src/pages/Applications.jsx
@@ -30,10 +30,19 @@ const Applications = ({ match }) => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col justify-start items-center pt-28 px-16  h-screen w-full overflow-y-scroll">
-        <div className="w-full p-20 relative grid grid-cols-3 gap-4">
-          {applications &&
-            applications.map((application, index) => (
+      <div className="flex flex-col justify-start items-center relative pt-28 px-16  h-screen w-full overflow-y-scroll">
+        <Link to="/dashboard" className="absolute top-25 left-10 z-40">
+          <Button
+            variant="outlined"
+            startIcon={<ArrowBackOutlinedIcon />}
+            className="!border-[1px] !border-gray-600 hover:!bg-transparent !text-black"
+          >
+            Go back
+          </Button>
+        </Link>
+        {applications.length > 0 ? (
+          <div className="w-full z-10 p-20 relative grid grid-cols-3 gap-4">
+            {applications.map((application, index) => (
               <ApplicationCard
                 applicantId={application.userId}
                 applicationStatus={application.status}
@@ -41,16 +50,12 @@ const Applications = ({ match }) => {
                 key={index}
               />
             ))}
-          <Link to="/dashboard" className="absolute top-0 left-0">
-            <Button
-              variant="outlined"
-              startIcon={<ArrowBackOutlinedIcon />}
-              className="!border-[1px] !border-gray-600 hover:!bg-transparent !text-black"
-            >
-              Go back
-            </Button>
-          </Link>
-        </div>
+          </div>
+        ) : (
+          <div className="text-2xl h-screen flex justify-center items-center font-medium  ">
+            <h1>No Applications found...</h1>
+          </div>
+        )}
       </div>
     </>
   );
