810e89718ba82e10dac2d59d6d0a0961a06cc3cd
tarunfy <tarunsharma8920@gmail.com>
2022-04-01 12:38:52 +0530

    changes in ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import Waves from "../assets/waves.svg";
 import candidate from "../assets/candidate.svg";
 import recruiter from "../assets/recruiter2.svg";
+import { IoArrowForwardCircle } from "react-icons/io5";
+import highlight from "../assets/highlight.svg";
 
 const Home = () => {
   return (
@@ -9,8 +11,12 @@ const Home = () => {
       <div id="blurry-gradient"></div>
       <div id="blurry-gradient2"></div>
       <div className="w-full flex justify-center flex-col bg-slate-50 items-center h-screen">
-        <h1 className="z-30 text-center text-7xl leading-[5.5rem] font-extrabold">
-          Direct Hiring App for Startup <br /> Ecosystem.
+        <h1
+          id="hero-heading"
+          className="z-30 text-center text-7xl leading-[5.5rem]"
+        >
+          Direct <span className="text-secondary-600">Hiring</span> App for
+          <span className="text-secondary-600"> Startup</span> <br /> Ecosystem.
         </h1>
         <p className="text-center cursor-default leading-7 text-lg z-30 font-light text-gray-700 my-10">
           Hirely, first job platform, connects startup founders, business
@@ -20,14 +26,21 @@ const Home = () => {
           <br />
           seekers and candidates.
         </p>
-        <Link
-          to="/signin"
-          className="group relative inline-flex border border-secondary-600 focus:outline-none  lg:ml-4 lg:inline-flex"
-        >
-          <span className="w-full inline-flex items-center justify-center self-stretch px-2 py-1 text-2xl text-secondary-600 text-center font-bold uppercase bg-white ring-1 ring-secondary-600 ring-offset-1 transform transition-transform group-hover:-translate-y-1 group-hover:-translate-x-1 group-focus:-translate-y-1 group-focus:-translate-x-1">
-            Get Started
-          </span>
-        </Link>
+        <div className="relative">
+          <Link
+            to="/signin"
+            className="group relative inline-flex border border-secondary-600 focus:outline-none  lg:ml-4 lg:inline-flex"
+          >
+            <span className="w-full inline-flex items-center justify-center self-stretch px-2 py-1 text-2xl text-secondary-600 text-center font-bold uppercase bg-white ring-1 ring-secondary-600 ring-offset-1 transform transition-transform group-hover:-translate-y-1 group-hover:-translate-x-1 group-focus:-translate-y-1 group-focus:-translate-x-1">
+              Get Started <IoArrowForwardCircle className="h-8 w-8 ml-1" />
+            </span>
+          </Link>
+          <img
+            src={highlight}
+            alt="highlight"
+            className="absolute animate-pulse -top-8 -right-8"
+          />
+        </div>
         <img
           src={Waves}
           alt="Waves"
